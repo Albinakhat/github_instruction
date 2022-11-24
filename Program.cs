2b3ffa2f6e189c2db0b9150c76e4474bbc7c3e54
Albinakhat <albilove@inbox.ru>
2022-11-25 00:30:00 +0300

    сщхранение 029

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которя на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("ВВедите число");
+Console.WriteLine("a =");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a ! == b)
+ 
+if ( a < b)
+ {
+ Console.WriteLine("aI < b " + a + " < " + b);
+ }
+     else
+ 
+    Console.WriteLine("a > b "+ a + " >" + b);
+  
+     else 
+  
+  {
+    Console.WriteLine("a - b "+ a + " - " +b);
+  }
